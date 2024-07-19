c96e0e3309ed21c36b8837556677fc3f9df2ceb9
EgorVidenin <egorvidenin@yandex.ru>
2024-07-19 14:23:25 +0400

    Removed it @EqualsAndHashCode

--- src/main/java/ru/practicum/shareit/item/dto/CommentDtoResponse.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentDtoResponse.java
@@ -1,17 +1,13 @@
 package ru.practicum.shareit.item.dto;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
-@NoArgsConstructor
 @Accessors(chain = true)
-@EqualsAndHashCode
 public class CommentDtoResponse {
 
     @NotNull
