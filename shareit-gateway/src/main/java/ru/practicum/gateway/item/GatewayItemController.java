bd424671040a34dc0f7510047f26c43ff9dac79b
EgorVidenin <egorvidenin@yandex.ru>
2024-07-27 15:30:32 +0300

    Error correction_6

--- shareit-gateway/src/main/java/ru/practicum/gateway/item/GatewayItemController.java
+++ shareit-gateway/src/main/java/ru/practicum/gateway/item/GatewayItemController.java
@@ -1,27 +1,19 @@
 package ru.practicum.gateway.item;
 
-import java.util.Collections;
-import javax.validation.Valid;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.gateway.item.dto.CommentDto;
 import ru.practicum.gateway.item.dto.ItemDto;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.Collections;
 
 @Controller
 @RequestMapping(path = "/items")
@@ -61,13 +53,14 @@ public class GatewayItemController {
 
     @GetMapping("/search")
     public ResponseEntity<Object> searchItems(@RequestHeader("X-Sharer-User-Id") long userId,
-                                              @RequestParam(required = false) String text,
+                                              @RequestParam String text,
                                               @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
                                               @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
         if (text.isBlank()) {
             log.info("Параметр 'text' пустой. Получен пустой список Item");
-            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
+            return ResponseEntity.ok(Collections.emptyList());
         }
+
         return itemClient.searchItems(userId, text, from, size);
     }
 
