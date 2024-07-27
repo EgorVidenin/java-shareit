07aed84105f9a1e3ec9380207bfdfb3df4cab780
EgorVidenin <egorvidenin@yandex.ru>
2024-07-27 16:05:57 +0300

    Error correction_9

--- shareit-gateway/src/main/java/ru/practicum/gateway/request/dto/ItemRequestDto.java
+++ shareit-gateway/src/main/java/ru/practicum/gateway/request/dto/ItemRequestDto.java
@@ -4,11 +4,13 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Accessors(chain = true)
 public class ItemRequestDto {
 
     @NotNull
+    @Size(min = 1, max = 250, message = "Description must be between 1 and 250 characters")
     private String description;
 }
